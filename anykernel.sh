ea8c4d1e6fe8e3594c744571711dcfdccfbc7a68
DarkLord1731 <darklord1731@gmail.com>
2018-07-29 00:10:59 +0530

    oxygen: Initialise Oxygen Kernel additions

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # begin properties
 properties() { '
 kernel.string=
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
@@ -13,7 +13,7 @@ device.name1=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/13540000.dwmmc0/by-name/BOOT;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -26,6 +26,18 @@ ramdisk_compression=auto;
 ## AnyKernel install
 dump_boot;
 
+
+# Enable Spectrum Support
+ui_print " ";
+ui_print "Enabling spectrum support...";
+replace_file sbin/spa 755 spectrum/spa;
+replace_file init.spectrum.rc 644 spectrum/init.spectrum.rc;
+insert_line init.samsungexynos7870.rc "import init.spectrum.rc" after "import init.fac.rc" "import init.spectrum.rc";
+ui_print " ";
+ui_print "Installing spectrum profiles...";
+rm -rf /data/media/0/Spectrum;
+cp -r /tmp/anykernel/Spectrum /data/media/0/Spectrum;
+chmod 777 /data/media/0/Spectrum/profiles;
 
 write_boot;
 
