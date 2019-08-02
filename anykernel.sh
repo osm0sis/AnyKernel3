5ad5d1ac3772b46dc9f1d8379ce7f32fb1a345bd
Albert I <kras@raphielgang.org>
2019-08-02 21:31:19 +0800

    ak3-core: Only unpack/repack ramdisk when it's exist
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Nexus Kernel By Prashant
+kernel.string=aRiseKernel By Prashant
 do.devicecheck=1
 do.modules=0
 do.systemless=1
