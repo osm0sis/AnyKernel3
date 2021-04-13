4810ea61b485cd85ba6199da4ff51bd112e5e65e
100Daisy <100Daisy@protonmail.com>
2021-04-13 21:05:34 +0200

    AnyKernel3: Deen-ify

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,19 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Stock+ by 100Daisy
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=deen
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
