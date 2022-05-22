b4f888116674d2f1b916dd1401b7e595e31dd1d0
100Daisy <48625180+100Daisy@users.noreply.github.com>
2022-05-22 10:31:46 +0200

    SUNBURN: Rebrand

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=DaisyKernel ACK by 100Daisy
+kernel.string=Sunburn by 100Daisy
 do.devicecheck=1
 do.modules=0
 do.systemless=1
