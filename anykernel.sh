0e137c4d2be9c3929eb73415e6ba209cbaeaf543
Ming Qingyan <81946319+xAsukax@users.noreply.github.com>
2023-06-26 15:07:41 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Kernel with su for OP4A7A
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=OP4A7A
+device.name2=kona
+device.name3=PDEM30
+device.name4=ossi
 device.name5=
 supported.versions=
 supported.patchlevels=
