49164ecaa73ac40b2a2d80ee37438ebfc3d7199a
IminatoPo <1300089753@qq.com>
2023-10-19 16:09:57 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -38,14 +38,11 @@ ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh && attributes;
+. tools/ak3-core.sh;
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
 
 
 # init.rc
